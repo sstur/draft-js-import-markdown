90ceea90e9f5eebe35f930f120505270ee17cbe8
Jakub Zmudzinski <jakub.zmudzinski@allegrogroup.com>
2016-05-18 08:01:12 +0200

    Browser parser instead of jsdom

--- src/__tests__/stateFromMarkdown-test.js
+++ src/__tests__/stateFromMarkdown-test.js
@@ -3,6 +3,9 @@ const {describe, it} = global;
 import expect from 'expect';
 import stateFromMarkdown from '../stateFromMarkdown';
 import {convertToRaw} from 'draft-js';
+import {jsdom} from 'jsdom';
+
+global.document = jsdom('');
 
 describe('stateFromMarkdown', () => {
   it('should create content state', () => {
@@ -22,6 +25,27 @@ describe('stateFromMarkdown', () => {
     let expectedEntitiesMap = {};
     expectedEntitiesMap[0] = {type: 'LINK', mutability: 'MUTABLE', data: {url: 'a.jpg'}};
     expect(rawContentState.entityMap).toEqual(expectedEntitiesMap);
+  });
+
+  it('should create content state from markdown quote', () => {
+    let markdown = '`test`';
+    let contentState = stateFromMarkdown(markdown);
+    let rawContentState = convertToRaw(contentState);
+    let blocks = removeKeys(rawContentState.blocks);
+    expect(blocks).toEqual(
+      [{text: 'test', type: 'unstyled', depth: 0, inlineStyleRanges: [{offset: 0, length: 4, style: 'CODE'}], entityRanges: []}]
+    );
+  });
+
+  it('should create content state from markdown code', () => {
+    let markdown = '> test';
+    let contentState = stateFromMarkdown(markdown);
+    console.log(contentState);
+    let rawContentState = convertToRaw(contentState);
+    let blocks = removeKeys(rawContentState.blocks);
+    expect(blocks).toEqual(
+      [{text: 'test', type: 'blockquote', depth: 0, inlineStyleRanges: [], entityRanges: []}]
+    );
   });
 
   it('should create content state from markdown image', () => {
--- src/stateFromMarkdown.js
+++ src/stateFromMarkdown.js
@@ -3,23 +3,13 @@
 import {stateFromElement} from 'draft-js-import-element';
 import remark from 'remark';
 import html from 'remark-html';
-import {jsdom} from 'jsdom';
+import parseHTML from './parseHTML';
 
 import type {ContentState} from 'draft-js';
 
 const markdownProcessor = remark().use(html);
-const document = jsdom('');
 
 export default function stateFromMarkdown(markdown: string): ContentState {
-  let html = markdownProcessor.process([
-    '---',
-    'remark:',
-    '  commonmark: true',
-    '---',
-    '',
-    markdown,
-  ].join('\n'));
-  let element = document.createElement('body');
-  element.innerHTML = html;
-  return stateFromElement(element);
+  let htmlString = markdownProcessor.process(markdown);
+  return stateFromElement(parseHTML(htmlString));
 }
